8e6fa32d10bb12bc0425e2878933ff5d96f94b32
何登锋 <hedengfeng@megvii.com>
2021-03-19 16:29:22 +0800

    增加workpoll

---
+++ workpool/def.go
@@ -0,0 +1,22 @@
+package workpool
+
+import (
+	"sync"
+	"time"
+)
+
+const MaxNum = 100
+
+// TaskHandler Define function callbacks
+type TaskHandler func() error
+
+// WorkPool serves incoming connections via a pool of workers
+type WorkPool struct {
+	closed       int32
+	isQueTask    int32         // Mark whether queue retrieval is task. 标记是否队列取出任务
+	errChan      chan error    // error chan
+	timeout      time.Duration // max timeout
+	wg           sync.WaitGroup
+	task         chan TaskHandler
+	waitingQueue *Queue
+}
