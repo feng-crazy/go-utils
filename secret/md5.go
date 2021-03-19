f155b1aff22d0920aac79a3e2576c0c88a06d4a4
何登锋 <hedengfeng@megvii.com>
2021-03-19 19:40:42 +0800

    增加md5加密通用函数

--- secret/md5.go
+++ secret/md5.go
@@ -9,16 +9,8 @@ import (
 // md5 加密
 func Md5Encryption(encryptionString string) string {
 	h := md5.New()
-	io.WriteString(h, encryptionString)
+	_, _ = io.WriteString(h, encryptionString)
 
 	pwmd5 := fmt.Sprintf("%x", h.Sum(nil))
-	/*
-		//salt1+MD5 pwd+salt2
-		io.WriteString(h, saltKey1)
-		io.WriteString(h, pwmd5)
-		io.WriteString(h, saltKey2)
-
-		last := fmt.Sprintf("%x", h.Sum(nil))
-	*/
 	return pwmd5
 }
