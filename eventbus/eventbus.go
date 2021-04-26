46a1649d3487df3bcf74e5a2e6a4e8d284634968
何登锋 <hedengfeng@megvii.com>
2021-04-26 11:53:08 +0800

    增加eventbus的通道形式的实现

---
+++ eventbus/eventbus.go
@@ -0,0 +1,45 @@
+package eventbus
+
+import "sync"
+
+type DataEvent struct {
+	Data  interface{}
+	Topic string
+}
+
+// DataChannel 是一个能接收 DataEvent 的 channel
+type DataChannel chan DataEvent
+
+// DataChannelSlice 是一个包含 DataChannels 数据的切片
+type DataChannelSlice []DataChannel
+
+// EventBus 存储有关订阅者感兴趣的特定主题的信息
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm          sync.RWMutex
+}
+
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+	if channels, found := eb.subscribers[topic]; found {
+		// 这样做是因为切片引用相同的数组，即使它们是按值传递的
+		// 因此我们正在使用我们的元素创建一个新切片，从而正确地保持锁定
+		channels := append(DataChannelSlice{}, channels...)
+		go func(data DataEvent, dataChannelSlices DataChannelSlice) {
+			for _, ch := range dataChannelSlices {
+				ch <- data
+			}
+		}(DataEvent{Data: data, Topic: topic}, channels)
+	}
+	eb.rm.RUnlock()
+}
+
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else {
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+	eb.rm.Unlock()
+}
---
+++ eventbus/eventbus_test.go
@@ -0,0 +1,47 @@
+package eventbus
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var eb = &EventBus{
+	subscribers: map[string]DataChannelSlice{},
+}
+
+func printDataEvent(ch string, data DataEvent) {
+	fmt.Printf("Channel: %s; Topic: %s; DataEvent: %v\n", ch, data.Topic, data.Data)
+}
+
+func publisTo(topic string, data string) {
+	for {
+		eb.Publish(topic, data)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+}
+
+func TestEventBus(t *testing.T) {
+	ch1 := make(chan DataEvent)
+	ch2 := make(chan DataEvent)
+	ch3 := make(chan DataEvent)
+
+	eb.Subscribe("topic1", ch1)
+	eb.Subscribe("topic2", ch2)
+	eb.Subscribe("topic2", ch3)
+
+	go publisTo("topic1", "Hi topic 1")
+	go publisTo("topic2", "Welcome to topic 2")
+
+	for {
+		select {
+		case d := <-ch1:
+			go printDataEvent("ch1", d)
+		case d := <-ch2:
+			go printDataEvent("ch2", d)
+		case d := <-ch3:
+			go printDataEvent("ch3", d)
+		}
+	}
+}
