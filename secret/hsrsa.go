9ba2d5b6ad0c36b971e63ec21066b58c5ed95d0a
何登锋 <hedengfeng@megvii.com>
2021-05-12 15:07:49 +0800

    RSA Private Key exposed

--- secret/hsrsa.go
+++ secret/hsrsa.go
@@ -10,11 +10,6 @@ import (
 	"fmt"
 )
 
-/**
-  RSA Demo[配置你可以提出来]
-  @author Bill
-*/
-
 var decrypted string
 
 func init() {
@@ -43,32 +38,17 @@ func usage() {
 }
 
 // 公钥和私钥可以从文件中读取
-var privateKey = []byte(`
------BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQCiAf7dzSYjXJQ28cqslEWj0Xj1C6zm1ccRAsYDesR/6y/Lbi2v
-IBdMLLin+jD+ogfdWN/4MUsY1wTf/WVRyQQdpZoUQgKSPBVES1tUSOEVVgEq8iAT
-k+5yycO2A00MnfqzFU9URYrReFleTfCIqUoVjSIhjtF+7uN9dO3AblAViwIDAQAB
-AoGAJgqr6y9J/lG15/T872tdsur0KS5VqlqWhPMOxjBkxdjW/0De71lsvxFdRuxj
-5tbrW9mLtf71MF8FIQeibAL0uRW1OFSS8PzCIeAlily74vfai1diZSnSScd91v1x
-daW8yrf9Jzn0fRTarN2Ivx2ELCu/vhAB+fdDXxoozmZdiXECQQDVG+EPX20bsJU+
-ukrLbMzjwQkzzv+HT9SPgOYzmj7JCBFrRYIwOKqJgRSrOFY/QIYR5jLKgmEjnPeg
-PCbc/U8ZAkEAwp01HUGnL5zRNqjQXCm2SxDiage9sSsGu+OmawfxGGV9iTxa34s6
-H9b6Zf2a5VmIB7QkZy8pFdzEMAXZIM2yQwJBAJBP3MxoNp61qZtc1CGFgAoLQowO
-9QNQkATNqRXwseu4xvL5kvvMo8+R1clM0TrsGt4sIOD6AYX4Wcfsc/TapKECQQC2
-laN1VVkzVF78dxk0vnWmq6qga67Of12L/aLum77Ycq96h8OCIy9fG+REt9ARCRSU
-la/pltdnCX6Ox43UNufdAkBb5Xb2HHiudJi2+HW347ohMu8j5io1xBxt1ICuVXFc
-tifJrsCvDQN96GxnPr+/koMYa6w1CUhjog68dVrZUm8q
------END RSA PRIVATE KEY-----
-`)
+var privateKey []byte
 
-var publicKey = []byte(`
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCiAf7dzSYjXJQ28cqslEWj0Xj1
-C6zm1ccRAsYDesR/6y/Lbi2vIBdMLLin+jD+ogfdWN/4MUsY1wTf/WVRyQQdpZoU
-QgKSPBVES1tUSOEVVgEq8iATk+5yycO2A00MnfqzFU9URYrReFleTfCIqUoVjSIh
-jtF+7uN9dO3AblAViwIDAQAB
------END PUBLIC KEY-----
-`)
+var publicKey []byte
+
+func SetPrivateKey(key string) {
+	privateKey = []byte(key)
+}
+
+func SetPublicKey(key string) {
+	publicKey = []byte(key)
+}
 
 // RsaEncrypt 加密
 func RsaEncrypt(origData []byte) ([]byte, error) {
