59e8c0f4256727eeeb1cc9eb29e426d938d71885
何登锋 <hedengfeng@megvii.com>
2021-07-01 19:29:50 +0800

    增加SingleMapKeyToStrArray

--- str/map.go
+++ str/map.go
@@ -1,8 +1,13 @@
 package str
 
-import "sort"
+import (
+	"errors"
+	"sort"
 
-func KeysOfMap(m map[string]string) []string {
+	"github.com/feng-crazy/go-utils/hmap"
+)
+
+func KeysOfMap(m map[string]interface{}) []string {
 	keys := make(sort.StringSlice, len(m))
 	i := 0
 	for key := range m {
@@ -12,4 +17,31 @@ func KeysOfMap(m map[string]string) []string {
 
 	keys.Sort()
 	return []string(keys)
+}
+
+func SingleMapKeyToStrArray(ms map[string]interface{}) []string {
+	keys := make([]string, len(ms))
+	for key := range ms {
+		keys = append(keys, key)
+	}
+
+	return keys
+}
+
+func SingleStructJsonTagToStrArray(st interface{}) ([]string, error) {
+	if st == nil {
+		return nil, errors.New("is nil")
+	}
+
+	ms, err := hmap.Struct2MapWithJson(st)
+	if err != nil {
+		return nil, errors.New("is nil")
+	}
+
+	keys := make([]string, len(ms))
+	for key := range ms {
+		keys = append(keys, key)
+	}
+
+	return keys, nil
 }
