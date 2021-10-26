e36add9941c95a8b03af583d4d37c4ceb21abab3
何登锋 <t-hedengfeng@megvii.com>
2021-10-26 17:36:18 +0800

    添加状态机模块
    Signed-off-by: 何登锋 <t-hedengfeng@megvii.com>

---
+++ statemachine/statemachine.go
@@ -0,0 +1,99 @@
+package statemachine
+
+import (
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type StateMachineState = string                   // 状态
+type StateMachineHandler func() StateMachineState // 处理方法，并返回新的状态
+
+type StateMachine struct {
+	mu       sync.Mutex                                // 排他锁
+	state    StateMachineState                         // 当前状态
+	handlers map[StateMachineState]StateMachineHandler // 每一个状态都可以出发有限个事件，执行有限个处理
+
+	tick   time.Duration
+	ticker *time.Ticker
+
+	stopC  chan struct{}
+	exited bool
+}
+
+// 获取当前状态
+func (sm *StateMachine) GetState() StateMachineState {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	return sm.state
+}
+
+// 设置当前状态
+func (sm *StateMachine) SetState(newState StateMachineState) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.state = newState
+}
+
+// 某状态添加事件处理方法
+func (sm *StateMachine) AddHandler(state StateMachineState, handler StateMachineHandler) *StateMachine {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	if sm.handlers == nil {
+		sm.handlers = make(map[StateMachineState]StateMachineHandler)
+	}
+
+	if _, ok := sm.handlers[state]; !ok {
+		sm.handlers[state] = handler
+	}
+
+	return sm
+}
+
+// 事件处理
+func (sm *StateMachine) Call() {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	handle := sm.handlers[sm.state]
+	if handle == nil {
+		logrus.Error("状态处理函数没有添加")
+		return
+	}
+	state := handle()
+	sm.state = state
+	return
+}
+
+func (sm *StateMachine) Start() {
+	sm.exited = false
+	sm.ticker = time.NewTicker(sm.tick)
+
+	go func() {
+		for !sm.exited {
+			select {
+			case <-sm.ticker.C:
+				sm.Call()
+			case <-sm.stopC:
+				sm.exited = true
+				sm.ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (sm *StateMachine) Stop() {
+	sm.stopC <- struct{}{}
+}
+
+// 实例化
+func NewStateMachine(initState StateMachineState, tick time.Duration) *StateMachine {
+	return &StateMachine{
+		state:    initState,
+		handlers: make(map[StateMachineState]StateMachineHandler),
+		tick:     tick,
+		stopC:    make(chan struct{}),
+	}
+}
---
+++ statemachine/statemachine_test.go
@@ -0,0 +1,79 @@
+package statemachine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	InitDriverState     = StateMachineState("初始化")
+	RegisterDriverState = StateMachineState("注册")
+	SyncChannelState    = StateMachineState("同步通道")
+	SyncDeviceState     = StateMachineState("同步设备")
+	WaitConnectState    = StateMachineState("等待连接状态")
+	RunState            = StateMachineState("正常运行")
+	StopState           = StateMachineState("停止运行")
+
+	InitDriverHandle = func() StateMachineState {
+		logrus.Info("InitDriverHandle...")
+		return WaitConnectState
+	}
+
+	WaitConnectHandle = func() StateMachineState {
+		logrus.Info("WaitConnectHandle...")
+		time.Sleep(1 * time.Second)
+		return RegisterDriverState
+	}
+
+	RegisterDriverHandle = func() StateMachineState {
+		logrus.Info("RegisterDriverHandle...")
+		return SyncChannelState
+	}
+
+	SyncChannelHandle = func() StateMachineState {
+		logrus.Info("SyncChannelHandle...")
+		return SyncDeviceState
+	}
+
+	SyncDeviceHandle = func() StateMachineState {
+		logrus.Info("SyncDeviceHandle...")
+		return RunState
+	}
+
+	RunHandle = func() StateMachineState {
+		logrus.Info("RunHandle...")
+		return RunState
+	}
+
+	StopHandle = func() StateMachineState {
+		logrus.Info("StopHandle...")
+		return StopState
+	}
+)
+
+func TestStateMachine(t *testing.T) {
+	sm := NewStateMachine(InitDriverState, 1*time.Second)
+
+	sm.AddHandler(InitDriverState, InitDriverHandle)
+	sm.AddHandler(RegisterDriverState, RegisterDriverHandle)
+	sm.AddHandler(SyncChannelState, SyncChannelHandle)
+	sm.AddHandler(SyncDeviceState, SyncDeviceHandle)
+	sm.AddHandler(WaitConnectState, WaitConnectHandle)
+	sm.AddHandler(RunState, RunHandle)
+	sm.AddHandler(StopState, StopHandle)
+
+	sm.Start()
+	select {
+	case <-time.After(10 * time.Second):
+		sm.SetState(StopState)
+	}
+
+	time.Sleep(3 * time.Second)
+	sm.Stop()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("exit")
+}
